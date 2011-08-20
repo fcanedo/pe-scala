1fdbb808191872b8dde92986b92c5bd7eced4bf2
Francisco José Canedo Dominguez <paco+git@lunatech.com>
2011-08-20 23:29:20 +0200

    Added some more explanation for problem 15

--- src/main/scala/com/github/fcanedo/pescala/Problem015.scala
+++ src/main/scala/com/github/fcanedo/pescala/Problem015.scala
@@ -2,6 +2,13 @@ package com.github.fcanedo.pescala
 
 /*
  * Starting in the top left corner in a 20 by 20 grid, how many routes are there to the bottom right corner?
+ *
+ * This problem reduces to: a bag containing 20 blue balls and 20 red balls, how many permutations of the
+ * balls are there if you pull out all the balls randomly?
+ *
+ * Solution: There are 40 balls in total, 40! would be the number of permutations for 40 unique balls. Since
+ * there are 2 sets of 20 balls that are equal to each other (ie. a blue ball is a blue ball), there are
+ * 20! * 20! permutations that are equal to each other. Therefore: 40! / 20! / 20!
  */
 object Problem015 extends EulerProblem {
 	def solve(x: Int) = {
